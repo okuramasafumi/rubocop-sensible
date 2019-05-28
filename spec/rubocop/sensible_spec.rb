76d05691927f4125e96cccf5a38989085a62b71e
OKURA Masafumi <masafumi.o1988@gmail.com>
2019-05-28 19:58:52 +0900

    Fix spec

--- spec/rubocop/sensible_spec.rb
+++ spec/rubocop/sensible_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Rubocop::Sensible do
   it "has a version number" do
     expect(Rubocop::Sensible::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
